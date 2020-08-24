b30a0d82359688a0dbbcaabca28bf054364a0036
felipeoliveiranet <felipeoliveiranet@gmail.com>
2020-08-24 19:32:45 -0300

    update: id string

--- html.js
+++ html.js
@@ -4,7 +4,6 @@ function createTableViewCellWithItemWith(id, description) {
     var tableViewCellWithItem  = '<div class="tableViewCell" id="tableViewCellDorItem_'+id+'">';
         tableViewCellWithItem += '<input class="input" id="input_'+id+'" type="text" placeholder="Novo item..." value="'+description+'"';
         tableViewCellWithItem += "oninput=\"updateItemTimeout('"+id+"', this)\">";
-        tableViewCellWithItem += '>';
         tableViewCellWithItem += "<div class=\"deleteButton\" onclick=\"deleteItem('" + id + "', this)\"></div>";
         tableViewCellWithItem += '<div class="divisor"></div>';
         tableViewCellWithItem += '</div>';
